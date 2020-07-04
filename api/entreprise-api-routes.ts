f76c6a60b34cf1b538cf91c7e5492edbc04b3250
Camille <camille.legeay@gmail.com>
2020-07-04 13:26:09 +0200

    entrepriseApiRoutes recup Array<EtablissementEntrant>

--- api/entreprise-api-routes.ts
+++ api/entreprise-api-routes.ts
@@ -4,6 +4,7 @@ import { EtablissementEntrant } from '../data/etablissementEntrant';
 import { Builder } from '../services/builder';
 import { ReponseApiEtablissements } from '../data/reponsesAPI';
 import axios from 'axios';
+import { EtablissementSortant } from '../data/etablissementSortant';
 
 export const apiRouter = Router();
 
@@ -14,9 +15,7 @@ export const apiRouter = Router();
 const urlEtablissement: string = 'https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/';
 const urlUniteLegale: string = 'https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/';
 //var _headers :string = "headers: {'Content-Type': 'application/json'})"; 
-
-
-    
+var dateHier = "2020-04-03"; //mise à jour dans la méthode de remplissage de mongoDB
     
 
     //get https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/?date_creation=2020-06-01
@@ -25,17 +24,21 @@ const urlUniteLegale: string = 'https://entreprise.data.gouv.fr/api/sirene/v3/un
     // 8931 entites créées
 
     ///////////////Partie gestion de mongoDB///////////////
+    //methode lancement mise à jour de mongoDB
+        //calculer la date d'hier au format AAAA-MM-jj
+        //lancer la suppression des entreprise créée il y a plus de x jours dans mongoDB
+        //lancer le remplissage de mongoDB avec les entreprises créées ou modifiée hier
 
-    //methode remplissage de mongoDB
-    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //methode remplissage de mongoDB 
     apiRouter.route('/test')
     .get(async function (req : Request, res: Response){
-        //test
-        let resultat : ReponseApiEtablissements = await findOnePageEtablissements();
-        res.send(JSON.stringify(resultat));
-        
-        
         //get etablissements crees hier sur l'API gouv de toutes les pages
+        let resultat : Array<EtablissementEntrant> = await findAllPagesEtablissements();
+        res.send("ok");
+        console.log(JSON.stringify(resultat[0]));
+        console.log(JSON.stringify(resultat[20]));
         //les transformer en etablissementSortant et les enregistrer dans mongoDB
         
     })
@@ -43,22 +46,32 @@ const urlUniteLegale: string = 'https://entreprise.data.gouv.fr/api/sirene/v3/un
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     //methode get etablissements crees hier sur l'API gouv de toutes les pages////////////////////////////////////////
-    //async function findAllPagesEtablissements() {
-    // let page : number = 1;
-    // var  listeEtablissements : Array<Etablissement> = new Array();
-    // do {
-    //    let reponse = findOnePageEtablissements(page);
-    //    setTimeout(function(){page+=},1000);
-    //    listeEtablissements.push(response.liste des établissements)
-    //  }   
-    //while (response.meta.total_pages>response.meta.page);
-    //}
+    async function findAllPagesEtablissements() : Promise<EtablissementEntrant[]>{
+     let page : number = 1;
+     let reponse : ReponseApiEtablissements;
+     var  listeEtablissements : Array<EtablissementEntrant> = new Array<EtablissementEntrant>();
+     do {
+        reponse = await findOnePageEtablissements(page);
+        await new Promise(r => setTimeout(r, 500));
+        page++;
+        listeEtablissements = listeEtablissements.concat(reponse.etablissements);
+        console.log("page récupérée : "+reponse.meta.page);
+        }   
+        while (reponse.meta.total_pages>reponse.meta.page);
+        //console.log(listeEtablissements[0]);
+        return listeEtablissements;
+    }
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     //methode get etablissements crees hier sur l'API gouv d'une page/////////////////////////////////////////////////
-    async function findOnePageEtablissements() {   
+    /**
+     * provide a page of results from the API SIRENE for business created yesterday
+     * no treatment because we need the meta in other function to know the number of pages
+     * @param numeroPage 
+     */
+    async function findOnePageEtablissements(numeroPage : number) : Promise<ReponseApiEtablissements> {   
         try{
-            let reponseJSON : ReponseApiEtablissements = (await axios.get(urlEtablissement/*+"?page="+numeroPage+"&date_creation="+new Date()*/)).data;
+            let reponseJSON : ReponseApiEtablissements = (await axios.get(urlEtablissement+"?page="+numeroPage+"&per_page=100"+"&date_creation="+dateHier)).data;
             return reponseJSON;
         }
         catch(e){
--- dist/api/entreprise-api-routes.js
+++ dist/api/entreprise-api-routes.js
@@ -12,37 +12,53 @@ exports.apiRouter = express_1.Router();
 const urlEtablissement = 'https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/';
 const urlUniteLegale = 'https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/';
 //var _headers :string = "headers: {'Content-Type': 'application/json'})"; 
+var dateHier = "2020-04-03"; //mise à jour dans la méthode de remplissage de mongoDB
 //get https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/?date_creation=2020-06-01
 // 13 426 entreprises créées
 //get https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/?date_creation=2020-06-01
 // 8931 entites créées
 ///////////////Partie gestion de mongoDB///////////////
-//methode remplissage de mongoDB
+//methode lancement mise à jour de mongoDB
+//calculer la date d'hier au format AAAA-MM-jj
+//lancer la suppression des entreprise créée il y a plus de x jours dans mongoDB
+//lancer le remplissage de mongoDB avec les entreprises créées ou modifiée hier
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//methode remplissage de mongoDB 
 exports.apiRouter.route('/test')
     .get(async function (req, res) {
-    //test
-    let resultat = await findOnePageEtablissements();
-    res.send(JSON.stringify(resultat));
     //get etablissements crees hier sur l'API gouv de toutes les pages
+    let resultat = await findAllPagesEtablissements();
+    res.send("ok");
+    console.log(JSON.stringify(resultat[0]));
+    console.log(JSON.stringify(resultat[20]));
     //les transformer en etablissementSortant et les enregistrer dans mongoDB
 });
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //methode get etablissements crees hier sur l'API gouv de toutes les pages////////////////////////////////////////
-//async function findAllPagesEtablissements() {
-// let page : number = 1;
-// var  listeEtablissements : Array<Etablissement> = new Array();
-// do {
-//    let reponse = findOnePageEtablissements(page);
-//    setTimeout(function(){page+=},1000);
-//    listeEtablissements.push(response.liste des établissements)
-//  }   
-//while (response.meta.total_pages>response.meta.page);
-//}
+async function findAllPagesEtablissements() {
+    let page = 1;
+    let reponse;
+    var listeEtablissements = new Array();
+    do {
+        reponse = await findOnePageEtablissements(page);
+        await new Promise(r => setTimeout(r, 500));
+        page++;
+        listeEtablissements = listeEtablissements.concat(reponse.etablissements);
+        console.log("page récupérée : " + reponse.meta.page);
+    } while (reponse.meta.total_pages > reponse.meta.page);
+    //console.log(listeEtablissements[0]);
+    return listeEtablissements;
+}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //methode get etablissements crees hier sur l'API gouv d'une page/////////////////////////////////////////////////
-async function findOnePageEtablissements() {
+/**
+ * provide a page of results from the API SIRENE for business created yesterday
+ * no treatment because we need the meta in other function to know the number of pages
+ * @param numeroPage
+ */
+async function findOnePageEtablissements(numeroPage) {
     try {
-        let reponseJSON = (await axios_1.default.get(urlEtablissement /*+"?page="+numeroPage+"&date_creation="+new Date()*/)).data;
+        let reponseJSON = (await axios_1.default.get(urlEtablissement + "?page=" + numeroPage + "&per_page=100" + "&date_creation=" + dateHier)).data;
         return reponseJSON;
     }
     catch (e) {
